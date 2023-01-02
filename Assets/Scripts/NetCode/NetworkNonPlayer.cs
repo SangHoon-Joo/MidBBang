c13a6eeb205517584df971fc4042d114c61b91f0
sanghoon-joo <sanghoon.joo@krafton.com>
2023-01-02 20:27:46 +0900

    미니언AI 작업중에 한번 올림

--- Assets/Scripts/NetCode/NetworkNonPlayer.cs
+++ Assets/Scripts/NetCode/NetworkNonPlayer.cs
@@ -2,25 +2,59 @@ using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Assertions;
-
+using ProjectWilson.Navigation;
+using System.Threading.Tasks;
 
 namespace ProjectWilson
 {
     public class NetworkNonPlayer : NetworkCharacter
     {
+        public enum Side
+        {
+            Red = 0,
+            Blue,
+            None,
+            Max
+        }
+
         [SerializeField]
-        private NavMeshAgent _NavMeshAgent;
+        private NonPlayerAI _NonPlayerAI;
+        private Side _Side;
+        public Side CurrentSide
+        {
+            get => _Side;
+            set {
+                if (!_InitializedSide) { _InitializedSide = true; }
+                _Side = value;
+            }
+        }
+        
+        private bool _InitializedSide;
 
         private void awake()
         {
-            Assert.IsNotNull<NavMeshAgent>(_NavMeshAgent);
+            Assert.IsNotNull<NonPlayerAI>(_NonPlayerAI);
         }
-        public override void OnNetworkSpawn()
+        public override async void OnNetworkSpawn()
         {
             base.OnNetworkSpawn();
-            if (IsOwner)
+            if (IsServer)
             {
-                //_CharacterMovementController.runSpeed = _TableData.Speed;
+                int retryCount = 0;
+                for(;;)
+                {
+                    if(_InitializedSide)
+                        break;
+
+                    retryCount++;
+                    if(retryCount >= 50)
+                    {
+                        Debug.LogError("_InitializedSide is false!!!");
+                        return;
+                    }
+                    await Task.Delay(100);
+                }
+                _NonPlayerAI.Init(_Side, _TableData.Speed, _TableData.Sight);
             }                
         }
 
