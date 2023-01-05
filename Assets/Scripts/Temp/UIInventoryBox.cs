c9afd5f809993ada37890730d2593b78a4332ecf
sanghoon-joo <sanghoon.joo@krafton.com>
2023-01-05 21:42:58 +0900

    인벤토리 스크립트 이동

---
+++ Assets/Scripts/Temp/UIInventoryBox.cs
@@ -0,0 +1,86 @@
+﻿/*
+using UnityEngine;
+using UnityEngine.Assertions;
+using Mosframe;
+using System.Collections.Generic;
+
+
+namespace ProjectWilson
+{
+	public class UIInventoryBox : MonoBehaviour
+	{
+		[SerializeField]
+		protected DynamicGridVScrollView _DynamicGridVScrollView;
+		protected List<int> _ItemIDList = new List<int>();
+		protected List<UIInventoryBoxItem> _ItemList = new List<UIInventoryBoxItem>();
+		public UIInventoryBoxItem SelectedItem { protected set; get; }
+		
+		public void Subscribe(UIInventoryBoxItem item)
+		{
+			_ItemList.Add(item);
+		}
+
+		protected virtual void Awake()
+		{
+			Assert.IsNotNull<DynamicGridVScrollView>(_DynamicGridVScrollView);
+		}
+
+		protected virtual void OnEnable()
+		{
+			if(CameraManager.Instance != null)
+				CameraManager.Instance.Lock();
+		}
+
+		protected virtual void OnDisable()
+		{
+			if(CameraManager.Instance != null)
+				CameraManager.Instance.Unlock();
+		}
+
+		public virtual void Refresh()
+		{
+			ForceResetItems();
+			_ItemIDList.Clear();
+			_ItemList.Clear();
+		}
+
+		public virtual void OnItemEnter(UIInventoryBoxItem button)
+        {
+			if(button.ItemID == 0)
+				return;
+        }
+
+        public virtual void OnItemClick(UIInventoryBoxItem button)
+        {
+			if(button.ItemID == 0)
+				return;
+
+            SelectedItem = button;
+        }
+
+        public virtual void OnItemExit(UIInventoryBoxItem button)
+        {
+			if(string.IsNullOrEmpty(button.ItemName))
+				return;
+        }
+
+        protected void ForceResetItems()
+        {
+			SelectedItem = null;
+		}
+
+		public virtual int GetItemIDByIndex(int index)
+        {
+            if(index < 0 || index >= _ItemIDList.Count)
+               return 0;
+
+            return _ItemIDList[index];
+        }
+
+		public int GetItemIDListCount()
+        {
+            return _ItemIDList.Count;
+        }
+	}
+}
+*/
---
+++ Assets/Scripts/Temp/UIInventoryBoxItem.cs
@@ -0,0 +1,101 @@
+﻿/*
+using UnityEngine;
+using UnityEngine.UI;
+using Mosframe;
+using UnityEngine.Assertions;
+using UnityEngine.EventSystems;
+
+namespace ProjectWilson
+{
+	public class UIInventoryBoxItem : MonoBehaviour, IDynamicScrollViewItem, IBeginDragHandler, IEndDragHandler, IDragHandler, IPointerEnterHandler, IPointerClickHandler, IPointerExitHandler
+	{
+		[SerializeField]
+		private ScrollRect _ScrollRect;
+		protected bool _IsScrolling = false;
+		[SerializeField]
+		protected UIInventoryBox _UIInventoryBox;
+		[SerializeField]
+		protected AtlasImage _IconImage;
+		[SerializeField]
+		protected Text _CountText;
+		public int ItemID { get; protected set; }
+		public string ItemName { get; protected set; }
+		public string ItemDescription { get; protected set; }
+
+		protected virtual void Awake()
+		{
+			Assert.IsNotNull<ScrollRect>(_ScrollRect);
+			Assert.IsNotNull<UIInventoryBox>(_UIInventoryBox);
+		}
+		
+		public virtual void onUpdateItem( int index )
+		{
+			_UIInventoryBox.Subscribe(this);
+			ItemID = _UIInventoryBox.GetItemIDByIndex(index);
+			int totalIndexCount = _UIInventoryBox.GetItemIDListCount();
+
+			if(index >= totalIndexCount || !TableDataManager.Instance.Item.Data.ContainsKey(ItemID))
+			{
+				_IconImage.spriteName = "None";
+				_CountText.text = string.Empty;
+				ItemID = 0;
+				ItemName = string.Empty;
+				return;
+			}
+
+			_IconImage.spriteName = TableDataManager.Instance.Item.Data[ItemID].IconName;
+			int itemCount = GameDataManager.Instance.Player.GetItemCount(ItemID);
+			if(itemCount > 1)
+				_CountText.text = itemCount.ToString();
+			else
+				_CountText.text = string.Empty;
+			
+			ItemName = TableDataManager.Instance.Item.Data[ItemID].Name;
+			ItemDescription = TableDataManager.Instance.Item.Data[ItemID].Description;
+		}
+
+		public virtual void OnBeginDrag(PointerEventData eventData)
+		{
+			if(_ScrollRect != null && Mathf.Abs(eventData.delta.x) <= 5.0f)
+			{
+				_IsScrolling = true;
+				_ScrollRect.OnBeginDrag(eventData);
+				return;
+			}
+		}
+		public void OnDrag(PointerEventData eventData)
+		{
+			if(_IsScrolling)
+			{
+				_ScrollRect.OnDrag(eventData);
+				return;
+			}
+		}
+
+		public void OnEndDrag(PointerEventData eventData)
+		{
+			if(_IsScrolling)
+			{
+				_IsScrolling = false;
+				_ScrollRect.OnEndDrag(eventData);
+				return;
+			}
+		}
+
+		public virtual void OnPointerEnter(PointerEventData eventData)
+        {
+			_UIInventoryBox.OnItemEnter(this);
+        }
+
+		public virtual void OnPointerClick(PointerEventData eventData)
+        {
+            _UIInventoryBox.OnItemClick(this);
+        }
+
+		public virtual void OnPointerExit(PointerEventData eventData)
+        {
+            _UIInventoryBox.OnItemExit(this);
+        }
+    }
+}
+*/
